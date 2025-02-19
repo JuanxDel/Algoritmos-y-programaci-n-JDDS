be05ebcdf76144a79cdb76f629b06fa5a4adb667
JuanxDel <nauj.xd3@gmail.com>
2025-02-19 18:10:40 -0500

    Delete promedio_edades.py

--- promedio_edades.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Ingrese Edad #1: "))
-b = int(input("Ingrese Edad #2: "))
-c = int(input("Ingrese Edad #3: "))
-sum = (a+b+c)/3
-print ("El promedio de edad de las 3 personas es: ",sum)
